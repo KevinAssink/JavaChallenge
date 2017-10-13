6c9f47c3c79bec1ad62873979006036dd12a324b
KevinAssink <kevin.assink@student.rocvf.nl>
2017-10-13 12:34:09 +0200

    JAVA CHALLENGE

--- JavaChallenge.java
+++ JavaChallenge.java
@@ -1,11 +1,12 @@
 import java.applet.Applet;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class JavaChallenge extends Applet {
     int GewichtJer;
     int GewichtVal;
     int GewichtHans;
-    int NULL;
     int YVal;
     int YJer;
     int YHans;
@@ -31,47 +32,68 @@ public void init() {
     add (Hans);
 
 
-    Button Toon = new Button("Toon");
+    Button Show = new Button("Toon");
     ShowListener Sl = new ShowListener();
-    show.addActionListener ( Sl );
-    add (Toon);
+    Show.addActionListener ( Sl );
+    add (Show);
 }
 
 
 public void paint (Graphics g){
     g.setColor(Color.black);
-    g.drawLine(50,260,350,260);
-    g.drawLine(50, 40,50,260);
-    g.drawString("20", 30, 225);
+    g.drawLine(50,300,350,300);
+    g.drawLine(50, 40,50,300);
+    g.drawString("20", 30, 265);
+    g.drawLine(60,260, 50,260);
+    g.drawString("40", 30, 225);
     g.drawLine(60,220, 50,220);
-    g.drawString("40", 30, 185);
+    g.drawString("60", 30, 185);
     g.drawLine(60,180, 50,180);
-    g.drawString("60", 30, 145);
+    g.drawString("80", 30, 145);
     g.drawLine(60,140, 50,140);
-    g.drawString("80", 30, 105);
+    g.drawString("100", 25, 105);
     g.drawLine(60,100, 50,100);
-    g.drawString("100", 25, 65);
+    g.drawString("120",25, 65);
     g.drawLine(60,60, 50,60);
-
 
     //Val
     g.setColor(Color.black);
-    g.drawString("Valerie", 75,270);
+    g.drawString("Valerie", 75,300);
     g.setColor(Color.red);
     g.fillRect(70, YVal, 50, GewichtVal*2);
 
-    //Jer
+    //Je
     g.setColor(Color.black);
-    g.drawString("Jeroen", 145,270);
+    g.drawString("Jeroen", 145,300);
     g.setColor(Color.green);
     g.fillRect(140, YJer, 50, GewichtJer*2);
 
     //Hans
     g.setColor(Color.black);
-    g.drawString("Hans", 215,270);
+    g.drawString("Hans", 215,300);
     g.setColor(Color.cyan);
     g.fillRect(210, YHans, 50, GewichtHans*2);
 }
 
+
+class ShowListener implements ActionListener {
+    public void actionPerformed(ActionEvent e) {
+        int start = 300;
+
+
+        GewichtVal = Integer.parseInt(Valerie.getText());
+        YVal = start - GewichtVal*2;
+
+        GewichtJer = Integer.parseInt(Jeroen.getText());
+        YJer = start - GewichtJer*2;
+
+        GewichtHans = Integer.parseInt(Hans.getText());
+        YHans = start - GewichtHans*2;
+
+        repaint();
+
+
+    }
+}
 
 }
